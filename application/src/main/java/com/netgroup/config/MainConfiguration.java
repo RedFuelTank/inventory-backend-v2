386357d4325b23cb0770eaa220f130e1d871a34f
Danila Romanov <danila.romanof@gmail.com>
2023-02-20 21:15:27 +0200

    application

--- application/src/main/java/com/netgroup/config/MainConfiguration.java
+++ application/src/main/java/com/netgroup/config/MainConfiguration.java
@@ -1,7 +1,7 @@
 package com.netgroup.config;
 
-import com.netgroup.domain.client.ClientServiceImpl;
-import com.netgroup.domain.client.api.ClientService;
+import com.netgroup.usecase.client.ClientServiceImpl;
+import com.netgroup.usecase.client.api.ClientService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
