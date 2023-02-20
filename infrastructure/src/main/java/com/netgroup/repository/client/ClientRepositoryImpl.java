bbd6b21910e2442420adcaac7295b624859efc2f
Danila Romanov <danila.romanof@gmail.com>
2023-02-20 21:14:43 +0200

    ClientRepositoryImpl has been created

---
+++ infrastructure/src/main/java/com/netgroup/repository/client/ClientRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.netgroup.repository.client;
+
+import com.netgroup.entity.client.Business;
+import com.netgroup.entity.client.Representative;
+import com.netgroup.usecase.client.api.ClientRepository;
+
+public class ClientRepositoryImpl implements ClientRepository {
+
+    @Override
+    public Representative register(Representative representative) {
+        return null;
+    }
+
+    @Override
+    public Business registerBusiness(Business business) {
+        return null;
+    }
+}
