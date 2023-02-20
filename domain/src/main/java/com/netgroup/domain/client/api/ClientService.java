e6799c6e3df904f5dd349c299e13002bf35f78a0
Danila Romanov <danila.romanof@gmail.com>
2023-02-20 21:07:09 +0200

    Domain module has been split into use case module and entity module

--- domain/src/main/java/com/netgroup/domain/client/api/ClientService.java
+++
@@ -1,5 +0,0 @@
-package com.netgroup.domain.client.api;
-
-public interface ClientService {
-    String test();
-}
--- domain/src/main/java/module-info.java
+++
@@ -1,3 +0,0 @@
-module com.netgroup.domain {
-    exports com.netgroup.domain.client.api;
-}
