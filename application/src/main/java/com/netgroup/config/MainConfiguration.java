7d127fe67b6719b7fd5c41b7cfdfe844f0f9d3ee
Danila Romanov <danila.romanof@gmail.com>
2023-03-01 22:00:52 +0200

    ClientRepository has been added for ClientService

--- application/src/main/java/com/netgroup/config/MainConfiguration.java
+++ application/src/main/java/com/netgroup/config/MainConfiguration.java
@@ -1,6 +1,7 @@
 package com.netgroup.config;
 
 import com.netgroup.usecase.client.ClientServiceImpl;
+import com.netgroup.usecase.client.api.ClientRepository;
 import com.netgroup.usecase.client.api.ClientService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MainConfiguration {
     @Bean
-    public ClientService getClientService() {
-        return new ClientServiceImpl();
+    public ClientService clientService(ClientRepository clientRepository) {
+        return new ClientServiceImpl(clientRepository);
     }
 }
