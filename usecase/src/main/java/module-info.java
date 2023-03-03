fae4c4274ace667ffb615ae48139c1099697901d
Danila Romanov <danila.romanof@gmail.com>
2023-03-03 21:19:47 +0200

    Inventory system api has been provided for export

--- usecase/src/main/java/module-info.java
+++ usecase/src/main/java/module-info.java
@@ -2,4 +2,5 @@ module com.netgroup.usecase {
     requires lombok;
     requires com.netgroup.entity;
     exports com.netgroup.usecase.client.api;
+    exports com.netgroup.usecase.inventory_system.api;
 }
