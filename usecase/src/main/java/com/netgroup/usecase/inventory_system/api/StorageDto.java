5f1ce35bd2c159aba6b439211d0601038099e384
Danila Romanov <danila.romanof@gmail.com>
2023-03-03 21:17:14 +0200

    ItemDto and StorageDto for Item and Storage entities respectively

---
+++ usecase/src/main/java/com/netgroup/usecase/inventory_system/api/StorageDto.java
@@ -0,0 +1,16 @@
+package com.netgroup.usecase.inventory_system.api;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class StorageDto {
+    private Long id;
+    @NonNull
+    private String name;
+    private Long upperStorageId;
+}
