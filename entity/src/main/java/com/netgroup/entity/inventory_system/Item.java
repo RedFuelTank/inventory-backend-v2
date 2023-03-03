5bc5530360355b36b986d063c5f644d6d945be65
Danila Romanov <danila.romanof@gmail.com>
2023-03-03 20:23:53 +0200

    Entities of Inventory System have been added (Item and Storage) + entities are available for export

---
+++ entity/src/main/java/com/netgroup/entity/inventory_system/Item.java
@@ -0,0 +1,16 @@
+package com.netgroup.entity.inventory_system;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class Item {
+    private Long id;
+    @NonNull
+    private String name;
+    private Long storageId;
+}
--- entity/src/main/java/module-info.java
+++ entity/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.netgroup.entity {
     requires lombok;
     exports com.netgroup.entity.client;
+    exports com.netgroup.entity.inventory_system;
 }
