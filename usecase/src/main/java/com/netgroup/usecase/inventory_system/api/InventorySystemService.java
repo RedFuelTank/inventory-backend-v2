98511ad86b366830f4b1c45448f9e182b3fd13e5
Danila Romanov <danila.romanof@gmail.com>
2023-03-12 09:17:00 +0200

    getContent now return Page value List instead

--- usecase/src/main/java/com/netgroup/usecase/inventory_system/api/InventorySystemService.java
+++ usecase/src/main/java/com/netgroup/usecase/inventory_system/api/InventorySystemService.java
@@ -1,14 +1,14 @@
 package com.netgroup.usecase.inventory_system.api;
 
-import com.netgroup.entity.inventory_system.Storage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface InventorySystemService {
-    List<ItemDto> getStorageItemsBy(Optional<Long> storageId, String businessName);
+    Page<ItemDto> getStorageItemsBy(Optional<Long> storageId, String businessName, Pageable pageable);
 
-    List<StorageDto> getSubStoragesBy(Optional<Long> storageId, String businessName);
+    Page<StorageDto> getSubStoragesBy(Optional<Long> storageId, String businessName, Pageable pageable);
 
     StorageDto saveStorage(StorageDto storageDto, String businessName);
 
