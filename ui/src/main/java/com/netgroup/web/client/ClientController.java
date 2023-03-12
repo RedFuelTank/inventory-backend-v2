e4d3fe3148197ba59e1c1cbfc71fcb6370e06f81
Danila Romanov <danila.romanof@gmail.com>
2023-03-12 22:31:36 +0200

    Response status for registration use cases has been set to 201 (CREATED)

--- ui/src/main/java/com/netgroup/web/client/ClientController.java
+++ ui/src/main/java/com/netgroup/web/client/ClientController.java
@@ -6,10 +6,9 @@ import com.netgroup.usecase.client.api.ClientService;
 import com.netgroup.usecase.client.api.RepresentativeDto;
 import com.netgroup.usecase.payment.api.PaymentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,14 +17,13 @@ public class ClientController {
     private final PaymentService paymentService;
 
     @PostMapping("/user/registration")
+    @ResponseStatus(HttpStatus.CREATED)
     public RepresentativeDto register(@RequestBody RepresentativeDto representativeDto) {
-        System.out.println(representativeDto.getUsername());
-        System.out.println(representativeDto.getPassword());
-
         return clientService.registerUser(representativeDto);
     }
 
     @PostMapping("/business/registration")
+    @ResponseStatus(HttpStatus.CREATED)
     public BusinessDto registerBusiness(@RequestBody BusinessDto businessDto, Authentication auth) {
         BusinessDto business = clientService.registerBusiness(auth.getName(), businessDto);
         paymentService.createPayment(business.getName());
